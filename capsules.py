eb20b8ffc6250138f785b1cf24b38f19fa5cdb6a
mritunjaymusale <mritunjaymusale@gmail.com>
2018-09-05 19:20:26 +0530

    uses different technique to load data

--- capsules.py
+++ capsules.py
@@ -2,27 +2,23 @@ import torch
 from torch.autograd import Variable
 from torch import nn
 from torch.nn import functional
-import data_loader
 from numpy import newaxis
-
-
-class Convolution(nn.Module):
-    def __init__(self, ):
-        super(Convolution, self).__init__()
-        self.conv = nn.Conv2d(
-            in_channels=1, out_channels=3, kernel_size=5, stride=1)
-
-    def forward(self, x):
-        x = self.conv(x)
-        return x
+from norb_loader import NORB
+from torch.utils.data import DataLoader
 
 
 def main():
-    training_samples, training_labels = data_loader.loadTrainingSet()
-    testing_samples, testing_labels = data_loader.loadTestingSet()
-    # training_samples = training_samples[:, newaxis, :, :]
-    training_samples = torch.from_numpy(training_samples).cuda()
-  
+   
+    trainingdataset = DataLoader(NORB(training=True), batch_size=128)
+    conv1 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=5, stride=1)
+    relu = nn.ReLU()
+
+    # temporary testing
+    for index, (data, labels) in enumerate(trainingdataset):
+        x = conv1(data)
+        x = relu(x)
+        print(x)
+        # print(data.shape)
 
 
 if __name__ == '__main__':
